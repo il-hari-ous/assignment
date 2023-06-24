666c45d70fc847b5ea52d1f67b2ad1228260f195
HARIPRASAD V <120312783+il-hari-ous@users.noreply.github.com>
2023-06-24 18:58:23 +0530

    Update GradeCalculatorswitch.java

--- GradeCalculatorswitch.java
+++ GradeCalculatorswitch.java
@@ -1,1 +1,45 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+
+    public static void main(String args[]) {
+
+        int mark;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Marks for 5 Subjects: ");
+        mark = scanner.nextInt();
+
+        System.out.print("Student's Grade is ");
+
+        int grade = mark / 10; // Get the tens digit of the mark
+
+        switch (grade) {
+            case 10:
+                System.out.print("A");
+                break;
+            case 9:
+                System.out.print("A");
+                break;
+            case 8:
+                System.out.print("B");
+                break;
+            case 7:
+                System.out.print("C");
+                break;
+            case 6:
+                System.out.print("D");
+                break;
+            case 5:
+                System.out.print("E");
+                break;
+            default:
+                System.out.print("F");
+                break;
+        }
+
+        scanner.close();
+    }
+}
 
